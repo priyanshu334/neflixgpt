faee28892c837f0ae894a1e0bea6aecf115060ff
priyanshu shrivastava <99priyanshu33@gmail.com>
2024-10-08 23:09:26 +0530

    2nd change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css'
 import React from 'react'
+import Body from './components/Body';
 
 const App = () =>{
   return (
-    <div className='bg-black '>
-      <h1 className='bg-black text-white'>
-        namaste netflix
-      </h1>
-    </div>
+    <Body/>
   )
 }
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+const Login = ()=>{
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default Login;
